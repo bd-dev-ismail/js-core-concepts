50d6e0e8cb88c4c43efdcf1abfbe35a03e19d592
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-05 20:07:03 +0600

    Leap Year Converter

---
+++ isLeapYear.js
@@ -0,0 +1,14 @@
+function isLeapYear(year){
+    const reminder = year % 4;
+    if(reminder === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+const myYearIsLeap = isLeapYear(1985);
+console.log('My Year is: ', myYearIsLeap);
+
+const herYearIsLeap = isLeapYear(2024);
+console.log('Her Year is: ', herYearIsLeap);
